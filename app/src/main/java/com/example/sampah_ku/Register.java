21552f6345480804220cfec5af6525be27909391
Denaldi Prima F <denaldiprimaf@gmail.com>
2021-05-17 14:13:22 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/sampah_ku/Register.java
@@ -0,0 +1,28 @@
+package com.example.sampah_ku;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.sampah_ku.databinding.ActivityRegisterBinding;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+
+}

5485a1edc37833567192376ce9a76689e2ce6d75
LuciousLawliet <irhamfadel@gmail.com>
2021-05-23 13:43:00 +0700

    Menambahkan Fungsi Perpindahan Navigasi Beranda, Transaksi, Sampah, dan Akun.

--- app/src/main/java/com/example/sampah_ku/Dashboard.java
+++ app/src/main/java/com/example/sampah_ku/Dashboard.java
@@ -1,14 +1,74 @@
 package com.example.sampah_ku;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Dashboard extends AppCompatActivity {
+
+    LinearLayout navTransac, navHome, navSampah, navAkun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        navTransac = (LinearLayout) findViewById(R.id.navTransac);
+        navHome = (LinearLayout) findViewById(R.id.navHome);
+        navSampah = (LinearLayout) findViewById(R.id.navSampah);
+        navAkun = (LinearLayout) findViewById(R.id.navAkun);
+
+        DahsboardHome dashboardhome = new DahsboardHome();
+        FragmentManager fm = getSupportFragmentManager();
+
+        fm.beginTransaction().add(R.id.mainLayout, dashboardhome).commit();
+
+        navTransac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DashboardTransaksi dashboardTransaksi = new DashboardTransaksi();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, dashboardTransaksi).commit();
+
+            }
+        });
+
+        navHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DahsboardHome dahsboardHome = new DahsboardHome();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, dahsboardHome).commit();
+
+            }
+        });
+
+        navSampah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DashboardSampah dashboardSampah = new DashboardSampah();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, dashboardSampah).commit();
+
+            }
+        });
+
+        navAkun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DashboardAkun dashboardAkun = new DashboardAkun();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, dashboardAkun).commit();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sampah_ku/TukarSampah.java
+++ app/src/main/java/com/example/sampah_ku/TukarSampah.java
@@ -3,62 +3,36 @@ package com.example.sampah_ku;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TukarSampah#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class TukarSampah extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TukarSampah() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TukarSampah.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TukarSampah newInstance(String param1, String param2) {
-        TukarSampah fragment = new TukarSampah();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button btnKembali;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tukar_sampah, container, false);
+        //return inflater.inflate(R.layout.fragment_tukar_sampah, container, false);
+        View v = inflater.inflate(R.layout.fragment_tukar_sampah, container, false);
+
+        btnKembali = v.findViewById(R.id.button8);
+
+        btnKembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DahsboardHome dahsboardHome = new DahsboardHome();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, dahsboardHome).commit();
+
+            }
+        });
+        return v;
     }
 }

72d3bf434e9167756f4f142d4643aa5ced3864c6
Denaldi Prima F <denaldiprimaf@gmail.com>
2021-05-25 04:41:08 +0700

    Tukar sampah > Layer Tukar Sampah -Popup daftar sampah -fungsionalitas datepicker -fungsionalitas foto > Layer konfirmasi sampah -navigasi kembali dan konfirmasi > Layer pesanan diterima navigasi kembali dan daftar transaksi

--- app/src/main/java/com/example/sampah_ku/KonfirmasiSampah.java
+++ app/src/main/java/com/example/sampah_ku/KonfirmasiSampah.java
@@ -3,62 +3,44 @@ package com.example.sampah_ku;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link KonfirmasiSampah#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class KonfirmasiSampah extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public KonfirmasiSampah() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment KonfirmasiSampah.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static KonfirmasiSampah newInstance(String param1, String param2) {
-        KonfirmasiSampah fragment = new KonfirmasiSampah();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Button konfirmasi, kembali;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_konfirmasi_sampah, container, false);
+        View v = inflater.inflate(R.layout.fragment_konfirmasi_sampah, container, false);
+
+        konfirmasi = v.findViewById(R.id.btnKonfirmasiTkrSampah);
+        kembali = v.findViewById(R.id.btnKembali);
+
+        konfirmasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PesananDiterima pesananDiterima = new PesananDiterima();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, pesananDiterima).commit();
+            }
+        });
+
+        kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TukarSampah tukarSampah = new TukarSampah();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, tukarSampah).commit();
+            }
+        });
+        return v;
+
     }
 }
--- app/src/main/java/com/example/sampah_ku/TukarSampah.java
+++ app/src/main/java/com/example/sampah_ku/TukarSampah.java
@@ -1,18 +1,45 @@
 package com.example.sampah_ku;
 
+import android.Manifest;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class TukarSampah extends Fragment {
 
-    Button btnKembali;
+    Button btnKembali, jenisSampah,btnFotoSampah, lanjut;
+    ImageView fotoSampah;
+    EditText txtSampah;
+//    String strtext;
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private TextView tvDateResult;
+    private Button btDatePicker;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,8 +48,65 @@ public class TukarSampah extends Fragment {
         //return inflater.inflate(R.layout.fragment_tukar_sampah, container, false);
         View v = inflater.inflate(R.layout.fragment_tukar_sampah, container, false);
 
-        btnKembali = v.findViewById(R.id.button8);
+        dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
 
+        tvDateResult = (TextView) v.findViewById(R.id.txtTanggal);
+        btDatePicker = (Button) v.findViewById(R.id.pilihTanggal);
+        txtSampah = v.findViewById(R.id.textSampah);
+        jenisSampah = v.findViewById(R.id.jenisSampah);
+        btnFotoSampah = v.findViewById(R.id.btnUnggahFoto);
+        fotoSampah = v.findViewById(R.id.fotosampah);
+        lanjut = v.findViewById(R.id.lanjut);
+
+
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    },
+                    100);
+        }
+
+//        try {
+//            strtext = getArguments().getString("jenisampah");
+//        }
+//        catch (Exception e){
+//            e.printStackTrace();
+//        }
+
+//        txtSampah.setText(strtext);
+        lanjut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                KonfirmasiSampah konfirmasiSampah = new KonfirmasiSampah();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, konfirmasiSampah).commit();
+            }
+        });
+        btnFotoSampah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,100);
+            }
+        });
+
+        btDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog();
+            }
+        });
+        jenisSampah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PopupJenisSampah.class);
+                startActivity(intent);
+            }
+        });
+
+        btnKembali = v.findViewById(R.id.button8);
         btnKembali.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,5 +118,51 @@ public class TukarSampah extends Fragment {
             }
         });
         return v;
+    }
+    private void showDateDialog(){
+
+        /**
+         * Calendar untuk mendapatkan tanggal sekarang
+         */
+        Calendar newCalendar = Calendar.getInstance();
+
+        /**
+         * Initiate DatePicker dialog
+         */
+        datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                /**
+                 * Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
+                 */
+
+                /**
+                 * Set Calendar untuk menampung tanggal yang dipilih
+                 */
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                /**
+                 * Update TextView dengan tanggal yang kita pilih
+                 */
+                tvDateResult.setText("Tanggal dipilih : "+dateFormatter.format(newDate.getTime()));
+            }
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        /**
+         * Tampilkan DatePicker dialog
+         */
+        datePickerDialog.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == 100){
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+            fotoSampah.setImageBitmap(captureImage);
+        }
     }
 }

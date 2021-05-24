e04b9e580504aa6b8c23a002fcfeb63cf2f76b60
LuciousLawliet <irhamfadel@gmail.com>
2021-05-25 05:29:02 +0700

    Menambahkan Navigasi Beli Produk Dan Tukar Poin Layout

--- app/src/main/java/com/example/sampah_ku/BeliProduk.java
+++ app/src/main/java/com/example/sampah_ku/BeliProduk.java
@@ -1,7 +1,11 @@
 package com.example.sampah_ku;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -9,10 +13,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class BeliProduk extends Fragment {
 
     Button beliKembali, beliProduk;
+    EditText namaPenerima, alamatPenerima;
+    private FirebaseUser pengguna;
+    private DatabaseReference reference;
+
+    private String userID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,10 +44,53 @@ public class BeliProduk extends Fragment {
 
         beliKembali = v.findViewById(R.id.btn_beli_kmbl);
         beliProduk = v.findViewById(R.id.btn_beli_psn_skrgn);
+        pengguna = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = pengguna.getUid();
 
         beliKembali.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                DahsboardHome dahsboardHome = new DahsboardHome();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, dahsboardHome).commit();
+
+            }
+        });
+
+        beliProduk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        User userProfile = snapshot.getValue(User.class);
+
+                        if (userProfile != null) {
+                            String poinC = userProfile.poinPengguna;
+
+                            Integer poinD = Integer.parseInt(poinC);
+
+                            poinD = poinD - 10000;
+
+                            String poinB = poinD.toString();
+
+                            HashMap hashMap = new HashMap();
+                            hashMap.put("poinPengguna", poinB);
+
+                            reference.child(userID).updateChildren(hashMap);
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        //Toast.makeText(UbahProfil.this, "Terdapat permasalahan!", Toast.LENGTH_LONG).show();
+                    }
+                });
 
                 DahsboardHome dahsboardHome = new DahsboardHome();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
--- app/src/main/java/com/example/sampah_ku/TukarPoinSaldoGopay.java
+++ app/src/main/java/com/example/sampah_ku/TukarPoinSaldoGopay.java
@@ -2,6 +2,7 @@ package com.example.sampah_ku;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -10,9 +11,23 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+
 public class TukarPoinSaldoGopay extends Fragment {
 
     Button btnKembali, btnTukar;
+    private FirebaseUser pengguna;
+    private DatabaseReference reference;
+
+    private String userID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +38,49 @@ public class TukarPoinSaldoGopay extends Fragment {
 
         btnKembali = v.findViewById(R.id.btn_gopay_btl);
         btnTukar = v.findViewById(R.id.btn_gopay_tkrPoin);
+        pengguna = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = pengguna.getUid();
+
+        btnTukar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        User userProfile = snapshot.getValue(User.class);
+
+                        if (userProfile != null) {
+                            String poinC = userProfile.poinPengguna;
+
+                            Integer poinD = Integer.parseInt(poinC);
+
+                            poinD = poinD - 10000;
+
+                            String poinB = poinD.toString();
+
+                            HashMap hashMap = new HashMap();
+                            hashMap.put("poinPengguna", poinB);
+
+                            reference.child(userID).updateChildren(hashMap);
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        //Toast.makeText(UbahProfil.this, "Terdapat permasalahan!", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                TukarPoinBerhasil tukarPoinBerhasil = new TukarPoinBerhasil();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.mainLayout, tukarPoinBerhasil).commit();
+
+            }
+        });
 
         btnKembali.setOnClickListener(new View.OnClickListener() {
             @Override
